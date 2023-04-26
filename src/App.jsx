ef81af5ab00b37e3e9ba2cd35c8642b4be632ec7
ShivamRaj-1 <shivamrajput1711@gmail.com>
2023-04-27 01:48:58 +0530

    npm error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 
 
 import './App.css'
-import Landing, {LandingFooter} from './pages/Landing/Landing'
+import Landing, { LandingFooter } from './pages/Landing/Landing'
 import Login from './pages/Login/Login'
 import SideNavbar from "./components/Sidebar/Components/sideNavbar";
 import SignUp from './pages/SignUp/SignUp'
 function App() {
 
- 
+
 
 
   return (
     <>
       <div className='main_background' >
-      <Landing />
-       <Login/>
+        <Landing />
+
+      </div>
+      <Login />
       <SideNavbar />
-        <SignUp/>
-    </div>
-    <LandingFooter/>
+      <SignUp />
+      <LandingFooter />
 
 
     </>
