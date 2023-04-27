5d864de7f4b23761f23536008d5bfaa592262107
sumns <sks23info@gmail.com>
2023-04-27 23:38:30 +0530

    for Tweet

---
+++ src/components/Sidebar/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase form 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAQkzNWCVB19VfuFJVNSzMsXLiBA45U8bM",
+    authDomain: "twitter-clone-67c8a.firebaseapp.com",
+    projectId: "twitter-clone-67c8a",
+    storageBucket: "twitter-clone-67c8a.appspot.com",
+    messagingSenderId: "300971692443",
+    appId: "1:300971692443:web:5649e5d67897886eb7d13e",
+    measurementId: "G-7Y3N4CMZCS"
+  };
+
+  const firebaseApp = firebase.initializApp
