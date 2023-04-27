b6f9aac5f4c630cf90047c8baef474fad427240e
shivi <tanushukla1332@gmail.com>
2023-04-27 23:38:05 +0530

    Create rightSide part of HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-
-
 import './App.css'
 import Landing, { LandingFooter } from './pages/Landing/Landing'
 import Login from './pages/Login/Login'
 import SideNavbar from "./components/Sidebar/Components/sideNavbar";
 import SignUp from './pages/SignUp/SignUp'
+import RigthSide from './components/RightSidebar/RigthSide';
 function App() {
-
-
-
 
   return (
     <>
       <div className='main_background' >
-      <Landing />
-       <Login/>
-      <SideNavbar />
-        <SignUp/>
-    </div>
-    <LandingFooter/>
-
-
+        {/* <Landing />
+        <Login />
+        <SideNavbar />
+        <SignUp /> */}
+      </div>
+      {/* <LandingFooter /> */}
+      <RigthSide/>
     </>
   );
 }
