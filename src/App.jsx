c71c42e80c466c7a7fcd938c890ceb67db5036b8
sumns <sks23info@gmail.com>
2023-04-28 16:05:15 +0530

    some changes folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 import './App.css'
 import SideNavbar from './components/Sidebar/sideNavbar'
+
+
 import Feed from './components/Sidebar/Feed'
 
 import Landing, { LandingFooter } from './pages/Landing/Landing'
 import Login from './pages/Login/Login'
-import SideNavbar from "./components/Sidebar/Components/sideNavbar";
+// import SideNavbar from "./components/Sidebar/Components/sideNavbar";
 import SignUp from './pages/SignUp/SignUp'
 import RigthSide from './components/RightSidebar/RigthSide';
 import Home from './pages/Home/Home'
