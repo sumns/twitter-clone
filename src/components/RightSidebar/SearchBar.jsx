1f95ecce9dc0fe24413add7257dd268b92af67a5
ShivamRaj-1 <shivamrajput1711@gmail.com>
2023-04-29 00:44:34 +0530

    left , center, right part marged

--- src/components/RightSidebar/SearchBar.jsx
+++ src/components/RightSidebar/SearchBar.jsx
@@ -1,24 +1,50 @@
-import React, {useState}  from 'react';
+import React, { useState } from 'react';
 import { FaSistrix } from "react-icons/fa";
+
 export default function SearchAppBar() {
-  const[inputfield,setInputField]=useState('')
+
+  const [inputfield, setInputField] = useState('')
+  
   return (
     <div>
-      <div>
-      <FaSistrix style={{
-            position:'absolute',
-            top: '2px',
-            left: '10px',
-            color: '#aaa',
-          }}/>
-          <input type="text" placeholder='Search' value={inputfield} onChange={(e)=>setInputField(e.target.value)} style={{
-            paddingLeft: '30px', 
-            width: '100%', 
-            border:"none",
-          }} />
+      <div style={{
+        marginBottom: "20px",
+        display: "flex",
+        alignItems: "center",
+
+        backgroundColor: '#f5f8fa',
+        padding: "15px",
+        borderRadius: '23px',
+
+        // "&:focus": {
+        //   background: '#fff',
+        //   border: "2px solid #1da1f2",
+        //   cursor: 'pointer',
+        // }
+
+      }} >
+        
+
+
+        <FaSistrix style={{color: 'gray'}} size={20}  />
+
+        <input
+          type="text"
+          placeholder='Search'
+          value={inputfield}
+          onChange={(e) => setInputField(e.target.value)}
+
+          style={{
+            paddingLeft: '30px',
+            // width: '100%',
+            border: "none",
+            outline: 'none',
+            background: 'transparent',
+          }}
+        />
       </div>
     </div>
-    
+
 
   );
 }
