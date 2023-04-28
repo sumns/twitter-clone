9b192ba7f9326122c08a4d1a372507f7997e357a
sumns <sks23info@gmail.com>
2023-04-28 17:21:44 +0530

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import './App.css'
 import SideNavbar from './components/Sidebar/sideNavbar'
 
 
-import Feed from './components/Sidebar/Feed'
+// import Feed from './components/Sidebar/Feed'
 
-import Landing, { LandingFooter } from './pages/Landing/Landing'
-import Login from './pages/Login/Login'
-// import SideNavbar from "./components/Sidebar/Components/sideNavbar";
-import SignUp from './pages/SignUp/SignUp'
+// import Landing, { LandingFooter } from './pages/Landing/Landing'
+// import Login from './pages/Login/Login'
+
+// import SignUp from './pages/SignUp/SignUp'
 import RigthSide from './components/RightSidebar/RigthSide';
 import Home from './pages/Home/Home'
 
